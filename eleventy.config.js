593c9d9ca671f92f89c29845fb36294108b36186
Eugene Arutyunov <e@bvz.name>
2025-07-07 10:46:43 +0300

    static build fix

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,13 @@
 module.exports = function (conf) {
-  conf.addPassthroughCopy('./src/index.css')
-  conf.addPassthroughCopy('./src/ids/colors.css')
-  conf.addPassthroughCopy('./src/ids/normalize.css')
-  conf.addPassthroughCopy('./src/ids/settings.css')
-  conf.addPassthroughCopy('./src/ids/layout.css')
-  conf.addPassthroughCopy('./src/ids/ids.css')
-  conf.addPassthroughCopy('./src/ids/gallery.css')
-  conf.addPassthroughCopy('./src/ids/navbar.css')
-  conf.addPassthroughCopy('./src/script.css')
-  conf.addPassthroughCopy('./src/index.js')
+  conf.addPassthroughCopy('./src/index.css');
+  conf.addPassthroughCopy('./src/ids/colors.css');
+  conf.addPassthroughCopy('./src/ids/normalize.css');
+  conf.addPassthroughCopy('./src/ids/settings.css');
+  conf.addPassthroughCopy('./src/ids/layout.css');
+  conf.addPassthroughCopy('./src/ids/ids.css');
+  conf.addPassthroughCopy('./src/ids/gallery.css');
+  conf.addPassthroughCopy('./src/ids/photoswipe.css');
+  conf.addPassthroughCopy('./src/index.js');
   conf.addPassthroughCopy('./src/assets');
   conf.addPassthroughCopy('./src/fonts');
 
